b85d1ff9edf2ac0a2a0611f81401fcec7a4409b3
Emerald <140569368+egolightly81@users.noreply.github.com>
2024-04-22 08:29:20 +0800

    added 7 js file

---
+++ script_6.js
@@ -0,0 +1,45 @@
+// Step 1
+let showActiveButton = document.getElementById("show-active")
+//Step 2
+function showActiveTasks() {
+  let tasks = document.getElementsByClassName("task");
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].classList.contains("completed")) {
+      tasks[i].style.display = "none";
+    } else {
+      tasks[i].style.display = "block";
+    }
+  }
+}
+// Step 3
+showActiveButton.addEventListener('click', showActiveTasks);
+
+
+// Step 1
+let showAllButton = document.getElementById("show-all")
+// Step 2
+function showAllTasks() {
+  let tasks = document.getElementsByClassName("task");
+  for (let i = 0; i < tasks.length; i++) {
+    tasks[i].style.display = "block";
+  }
+}
+// Step 3
+showAllButton.addEventListener('click', showAllTasks);
+
+
+
+/* Exercise/Homework to implement show only completed
+let showCompletedButton = document.getElementById("show-completed");
+
+function showCompletedTasks() {
+  let tasks = document.getElementsByClassName("task");
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].classList.contains("completed")) {
+      tasks[i].style.display = "block";
+    } else {
+      tasks[i].style.display = "none";
+    }
+  }
+}
+showCompletedButton.addEventListener('click', showCompletedTasks); */
