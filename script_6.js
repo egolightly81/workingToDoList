28e238b843cf3ed42bebd2334af716a8ac921fa4
Emerald <140569368+egolightly81@users.noreply.github.com>
2024-04-28 19:44:33 +0800

    reset js

--- script_6.js
+++ script_6.js
@@ -1,7 +1,6 @@
 // Step 1
-let showActiveButton = document.getElementById("show-active");
-
-// Step 2
+let showActiveButton = document.getElementById("show-active")
+//Step 2
 function showActiveTasks() {
   let tasks = document.getElementsByClassName("task");
   for (let i = 0; i < tasks.length; i++) {
@@ -12,14 +11,12 @@ function showActiveTasks() {
     }
   }
 }
-
 // Step 3
 showActiveButton.addEventListener('click', showActiveTasks);
 
 
 // Step 1
-let showAllButton = document.getElementById("show-all");
-
+let showAllButton = document.getElementById("show-all")
 // Step 2
 function showAllTasks() {
   let tasks = document.getElementsByClassName("task");
@@ -27,28 +24,8 @@ function showAllTasks() {
     tasks[i].style.display = "block";
   }
 }
-
 // Step 3
 showAllButton.addEventListener('click', showAllTasks);
-
-
-// Step 1
-let showCompletedButton = document.getElementById("show-completed");
-
-// Step 2
-function showCompletedTasks() {
-  let tasks = document.getElementsByClassName("task");
-  for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i].classList.contains("completed")) {
-      tasks[i].style.display = "block";
-    } else {
-      tasks[i].style.display = "none";
-    }
-  }
-}
-
-// Step 3
-showCompletedButton.addEventListener('click', showCompletedTasks);
 
 
 
